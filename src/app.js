9e8c26e6e5c7e13a8db909fe91423ee73a18df36
Giovanni Ferrara <gianmarco.ferrara@gmail.com>
2019-02-03 19:29:21 +0100

    install react-redux

--- src/app.js
+++ src/app.js
@@ -1,34 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
-import configStore from './store/configureStore';
-import {addExpense, editExpense, removeExpense} from './actions/expenses';
-import {setTextFilter, setEndDate, setStartDate, sortByAmount, sortByDate} from './actions/filters';
-import {getVisibleExpenses} from './selectors/expenses'
-const store = configStore();
 
-store.dispatch(addExpense({
-    description: "Water",
-    amount: 300
-}))
-store.dispatch(addExpense({
-    description: "Gatto",
-    amount: 100
-}))
-store.dispatch(addExpense({
-    description: "Cane",
-    amount: 900
-}))
+const store = configureStore();
 
-store.dispatch(sortByAmount())
-/*
-store.subscribe(() => {
-    const state = store.getState();
-    const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-    console.log(visibleExpenses);
-  });
-*/
-console.log(store.getState())
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+store.dispatch(addExpense({ description: 'Water bill' }));
+store.dispatch(addExpense({ description: 'Gas bill' }));
+store.dispatch(setTextFilter('water'));
+
+setTimeout(() => {
+  store.dispatch(setTextFilter('rent'));
+}, 3000)
+
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+console.log(visibleExpenses);
+
+const jsx = (
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
+);
+
+ReactDOM.render(jsx, document.getElementById('app'));
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,11 +1,14 @@
-import {createStore, combineReducers} from 'redux'
-import expensesReducer from '../reducers/expenses'
-import filtersReducer from '../reducers/filters'
+import { createStore, combineReducers } from 'redux';
+import expensesReducer from '../reducers/expenses';
+import filtersReducer from '../reducers/filters';
 
-export default ()=>{
-    const store = createStore(combineReducers({
-        expenses: expensesReducer,
-        filter: filtersReducer
-    }))
-    return store
-}
+export default () => {
+  const store = createStore(
+    combineReducers({
+      expenses: expensesReducer,
+      filters: filtersReducer
+    })
+  );
+
+  return store;
+};
